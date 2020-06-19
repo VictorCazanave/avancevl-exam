eb7359d1bad9763b586dbb13fcb5b7cb185de440
Victor Cazanave <victor.cazanave@gmail.com>
2020-06-19 16:40:16 +0800

    Add exercise form with example

--- src/components/Exercise/Exercise.js
+++ src/components/Exercise/Exercise.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ExerciseFilters from './ExerciseFilters';
 import ExerciseForm from './ExerciseForm';
+import ExerciseExample from './ExerciseExample';
 
 function Exercise() {
 	return (
 		<main className="exercise">
 			<ExerciseFilters />
-			<ExerciseForm />
+			<ExerciseForm title="Arithmetic - Real Problem">
+				<ExerciseExample />
+			</ExerciseForm>
 		</main>
 	)
 }
--- src/components/Exercise/ExerciseForm.js
+++ src/components/Exercise/ExerciseForm.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Button from 'react-bootstrap/Button';
 
-function ExerciseForm() {
+function ExerciseForm({ title, children }) {
 	return (
 		<form className="form">
-			Form
+			<h2 className="form__title">{title}</h2>
+			{children}
+			<footer className="form__footer">
+				<Button variant="outline-secondary" className="form__footer__skip">Skip</Button>
+				<Button variant="primary" type="submit">Submit</Button>
+			</footer>
 		</form>
 	)
 }
