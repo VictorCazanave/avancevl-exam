10d5379c5677d0d5c3a922e68f54bbd65255e559
Victor Cazanave <victor.cazanave@gmail.com>
2020-06-19 16:32:31 +0800

    Implement simple selector for desktop

--- src/components/Exercise/ExerciseFilters.js
+++ src/components/Exercise/ExerciseFilters.js
@@ -1,9 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 function ExerciseFilters() {
+	const [topic, setTopic] = useState('all');
+	const [topicOptions] = useState([
+		{
+			value: 'all',
+			label: 'All Topics',
+		},
+		{
+			value: 'algebra',
+			label: 'Algebra',
+		},
+		{
+			value: 'geometry',
+			label: 'Geometry',
+		},
+		{
+			value: 'trigonometry',
+			label: 'Trigonometry',
+		},
+		{
+			value: 'arithmetic',
+			label: 'Arithmetic',
+		},
+	]);
+
 	return (
 		<div className="filters">
-			Filters
+			<Dropdown onSelect={setTopic}>
+				<Dropdown.Toggle>
+					{topicOptions.find((option) => option.value === topic).label}
+				</Dropdown.Toggle>
+
+				<Dropdown.Menu>
+					<Dropdown.Toggle>
+						X
+					</Dropdown.Toggle>
+					<Dropdown.Header>TOPICS</Dropdown.Header>
+					{topicOptions.map((option) => (
+						<Dropdown.Item key={option.value} as="div" eventKey={option.value}>
+							{option.label}
+						</Dropdown.Item>
+					))}
+				</Dropdown.Menu>
+			</Dropdown>
 		</div>
 	);
 }
